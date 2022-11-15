4d54d7f58e296ac5ae20582815359475d9aaf042
Jorge Graça <j.graca@hotmail.com>
2022-11-15 18:45:51 +0000

    Filtering, sorting and pagination - Brands

--- controllers/brands.js
+++ controllers/brands.js
@@ -9,18 +9,73 @@ const asyncHandler = require('../middleware/async');
 exports.getBrands = asyncHandler( async (req, res, next) => {
   console.log(req.query);
 
-  let queryStr = JSON.stringify(req.query);
-  console.log(queryStr);
+  // Copy req.query
+  const reqQuery = { ...req.query };
+  console.log(req.query);
+  // Fields to exclude
+  const removeFields = ['select', 'sort', 'page', 'limit'];
+
+  // Loop over removeFields and delete them from reqQuery
+  removeFields.forEach(param => delete reqQuery[param]);
+
+  let queryStr = JSON.stringify(reqQuery);
+  
   //Acrescenta o $ na rota para usar as expressões
   queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`);
-  console.log(queryStr);
+
   let query;
   query = Brand.find(JSON.parse(queryStr));
+
+   // Select Fields
+   if (req.query.select) {
+    const fields = req.query.select.split(',').join(' ');
+
+    query = query.select(fields);
+  }
+
+   // Sort
+   if (req.query.sort) {
+    const sortBy = req.query.sort.split(',').join(' ');
+    query = query.sort(sortBy);
+  } else {
+    query = query.sort('-createdAt');
+  }
+
+// Pagination
+const page = parseInt(req.query.page, 10) || 1;
+const limit = parseInt(req.query.limit, 10) || 30;
+const startIndex = (page - 1) * limit;
+const endIndex = page * limit;
+const total = await Brand.countDocuments();
+
+query = query.skip(startIndex).limit(limit);
+
   const brands = await query;
+
+   // Pagination result (navegação entre páginas)
+   const pagination = {};
+
+   if (endIndex < total) {
+     pagination.next = {
+       page: page + 1,
+       limit
+     };
+   }
+
+   if (startIndex > 0) {
+     pagination.prev = {
+       page: page - 1,
+       limit
+     };
+   }
 
     res
       .status(200)
-      .json({ success: true, count: brands.length, data: brands });
+      .json({ success: true,
+      count: brands.length,
+      pagination,
+      data: brands
+    });
   
   });
 
